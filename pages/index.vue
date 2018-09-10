4ea74af47f03b603deb652fa3741da69b0bc0681
Cesar Bretana Gonzalez <bretanac@gmail.com>
2018-09-10 12:29:21 +0200

    Added github integration

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container">
+  </section>
+</template>
+
+<script>
+
+export default {
+
+  components: {
+  },
+  mounted() {
+    const {code} = this.$route.query;
+    this.$axios.get(`http://localhost:3000/auth/github?code=${code}`)
+      .then((resp) => {
+        console.log(resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+}
+</script>
