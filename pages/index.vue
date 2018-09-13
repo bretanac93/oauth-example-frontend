5c5e2cb7fca2e63e8785888808c9092fa3b0b55e
Cesar Bretana Gonzalez <bretanac@gmail.com>
2018-09-14 00:10:20 +0200

    Added anchor for authentication

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,25 @@
 <template>
   <section class="container">
+    <a href="https://github.com/login/oauth/authorize?scope=user:email&client_id=356b757559647d2a3fba">Login with Github</a>
   </section>
 </template>
 
 <script>
 
 export default {
-
   components: {
   },
   mounted() {
-    const {code} = this.$route.query;
-    this.$axios.get(`http://localhost:3000/auth/github?code=${code}`)
+    const {code} = this.$route.query; // Retrieve the code provided by the OAuth provider
+    if (code) {
+      this.$axios.get(`http://localhost:3000/auth/github?code=${code}`) // Send it to your server
       .then((resp) => {
-        console.log(resp.data);
+        console.log(resp.data); // Check your console :)
       })
       .catch((err) => {
         console.log(err);
       })
+    }
   }
 }
 </script>
